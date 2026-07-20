267c116300cff3df5db95ba1e8412a62975df590
JianPierDev <jianpierdev@gmail.com>
2026-07-20 17:55:25 -0500

    fix: update item count dynamically

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -7,14 +7,14 @@ type OrderItemProps = {
 
 export function OrderContents({ order, removeItem }: OrderItemProps) {
   return (
-    <div>
+    <div className="space-y-3">
       {order.map((item) => (
         <div key={item.id} className="flex justify-between">
           <p>
-            {item.name} - {item.price}
+            {item.name} - ${item.price * item.quantity}
           </p>
           <button
-            className="w-6 h-6 bg-red-500 rounded-full text-white font-bold"
+            className="w-6 h-6 bg-red-500 rounded-full text-white font-bold cursor-pointer"
             onClick={() => removeItem(item)}
           >
             X
